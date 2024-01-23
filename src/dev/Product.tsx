dca40e9ad2b12c4fa946b68f0702877ad73ffa94
F0gr1 <ishigami0314@icloud.com>
2024-01-23 23:46:06 +0900

    update fetchData

--- src/dev/Product.tsx
+++ src/dev/Product.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CardComponent from '../Components/util/CardComponents'
 import { styled } from 'styled-components'
 import { ProductItemsModel } from '../model/ProductItems';
@@ -14,27 +14,36 @@ margin-top: 1%;
 `;
 
 const Product = () => {
-  const products:  ProductItemsModel[] =[
-    { id : 1 , name:'くま' , price: 100 , description:'活発なくまさん' } ,
-    { id : 2 , name:'くま2' , price: 200 , description:'活発なくまさん' } ,
-    { id : 1 , name:'くま' , price: 100 , description:'活発なくまさん' } ,
-    { id : 2 , name:'くま2' , price: 200 , description:'活発なくまさん' } ,
-    { id : 1 , name:'くま' , price: 100 , description:'活発なくまさん' } ,
-    { id : 2 , name:'くま2' , price: 200 , description:'活発なくまさん' } ,
-    { id : 1 , name:'くま' , price: 100 , description:'活発なくまさん' } ,
-    { id : 2 , name:'くま2' , price: 200 , description:'活発なくまさん' } ,
-    { id : 1 , name:'くま' , price: 100 , description:'活発なくまさん' } ,
-    { id : 2 , name:'くま2' , price: 200 , description:'活発なくまさん' } ,
-    { id : 1 , name:'くま' , price: 100 , description:'活発なくまさん' } ,
-    { id : 2 , name:'くま2' , price: 200 , description:'活発なくまさん' } ,
-    { id : 1 , name:'くま' , price: 100 , description:'活発なくまさん' } ,
-    { id : 2 , name:'くま2' , price: 200 , description:'活発なくまさん' } ,
-    { id : 1 , name:'くま' , price: 100 , description:'活発なくまさん' } ,
-    { id : 2 , name:'くま2' , price: 200 , description:'活発なくまさん' } ,
-  ]
+  const [products, setProducts] = useState<ProductItemsModel[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  useEffect(() =>{
+    const fetchProductData = async() =>{
+      try{
+        const response = await fetch('http://localhost/products');
+        
+        if(!response.ok){
+          throw new Error('Error');
+        }
+
+        const result = await response.json();
+        const resultProducts = JSON.parse(result);
+        setProducts(resultProducts);
+      }catch(error:any){
+        setError(error);
+      }
+    }
+    fetchProductData();
+  }, []);
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!products) {
+    return <div>Loading...</div>;
+  }
   return (
     <ProductContainer>
-      {products.map((product: ProductItemsModel) =>{
+      {products && products.map((product: ProductItemsModel) =>{
           return (
             <CardComponent product = {product}/>
           )
